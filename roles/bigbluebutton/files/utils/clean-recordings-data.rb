4aaae68967f2acba5eee56e000d2db0999355aae
Felipe Cecagno <fcecagno@gmail.com>
2018-06-28 00:30:42 -0300

    - add missing documentation - hide network monitor - remove old kurento media files

--- roles/bigbluebutton/files/utils/clean-recordings-data.rb
+++ roles/bigbluebutton/files/utils/clean-recordings-data.rb
@@ -11,5 +11,7 @@ Dir.glob("/var/bigbluebutton/recording/status/published/*.done").each do |publis
   FileUtils.rm_rf "/var/bigbluebutton/#{record_id}"
   FileUtils.rm_rf "/usr/share/red5/webapps/video/streams/#{record_id}"
   FileUtils.rm_rf "/usr/share/red5/webapps/screenshare/streams/#{record_id}"
+  FileUtils.rm_rf "/var/kurento/recordings/#{record_id}"
+  FileUtils.rm_rf "/var/kurento/screenshare/#{record_id}"
   Dir.glob("/var/freeswitch/meetings/#{record_id}-*.wav").each { |file| FileUtils.rm_rf file }
 end
