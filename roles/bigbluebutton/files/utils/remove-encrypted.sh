e5f231ef7835875b2acd3beb1e801cbc59827138
Felipe Cecagno <fcecagno@gmail.com>
2018-09-05 13:27:23 -0300

    add cronjob to remove mconf_encrypted after a few days

---
+++ roles/bigbluebutton/files/utils/remove-encrypted.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+encrypted_days=7
+
+if [ -d /var/bigbluebutton/published/mconf_encrypted ]; then
+    find /var/bigbluebutton/published/mconf_encrypted/ -maxdepth 1 -mtime +$encrypted_days -exec rm -rf {} \;
+fi
+
+if [ -d /var/bigbluebutton/unpublished/mconf_encrypted ]; then
+    find /var/bigbluebutton/unpublished/mconf_encrypted/ -maxdepth 1 -mtime +$encrypted_days -exec rm -rf {} \;
+fi
