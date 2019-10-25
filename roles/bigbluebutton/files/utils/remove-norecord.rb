0fe3b74236e582beb5247ae931f02a2fb4c327a7
Felipe Cecagno <fcecagno@gmail.com>
2019-10-24 22:23:47 -0300

    move yesterday calculation to outside of the loop

--- roles/bigbluebutton/files/utils/remove-norecord.rb
+++ roles/bigbluebutton/files/utils/remove-norecord.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
 # encoding: UTF-8
 
+yesterday = Time.now - (60 * 60 * 24)
 Dir.glob("/var/bigbluebutton/recording/status/archived/*.norecord").each do |norecorded|
   match = /^(\w+-\d+)\.norecord$/.match File.basename(norecorded)
-  yesterday = Time.now - (60 * 60 * 24)
   next if match.nil? || File.mtime(norecorded) > yesterday
   record_id = match[1]
 
