b316b4abf93de3da964ed02f4c024c9d348e900f
Felipe Cecagno <fcecagno@gmail.com>
2020-07-23 23:58:05 -0300

    fix sipp check, and minor changes

--- roles/bigbluebutton-prometheus/files/freeswitch_sipp_check.sh
+++ roles/bigbluebutton-prometheus/files/freeswitch_sipp_check.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 IP=$1
 PORT=$2
+VIDEO_PORT=$3
 
-R=$(/usr/bin/sipp -sn uac -m 1 -aa -nostdin -s sippcheck "${IP}:${PORT}" > /dev/null 2>&1)
+R=$(timeout 5 /usr/bin/sipp -sn uac -m 1 -aa -i ${IP} -mp ${VIDEO_PORT} -nostdin -s sippcheck "${IP}:${PORT}" > /dev/null 2>&1)
 CHECK_STATUS=$(($? == 0))
 cat << EOF
 # HELP bbb_freeswitch_sipp_status FreeSWITCH negotation health status via sipp
--- roles/bigbluebutton/files/utils/remove-encrypted.sh
+++ roles/bigbluebutton/files/utils/remove-encrypted.sh
@@ -2,14 +2,6 @@
 
 encrypted_days=7
 
-if [ -d /var/bigbluebutton/published/mconf_encrypted/ ]; then
-    find /var/bigbluebutton/published/mconf_encrypted/ -maxdepth 1 -mtime +60 -exec rm -rf {} \;
-fi
-
-if [ -d /var/bigbluebutton/unpublished/mconf_encrypted/ ]; then
-    find /var/bigbluebutton/unpublished/mconf_encrypted/ -maxdepth 1 -mtime +60 -exec rm -rf {} \;
-fi
-
 if [ -d /var/bigbluebutton/deleted/mconf_encrypted/ ]; then
     find /var/bigbluebutton/deleted/mconf_encrypted/ -maxdepth 1 -mtime +$encrypted_days -exec rm -rf {} \;
 fi
