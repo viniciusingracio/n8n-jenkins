0eb2ff804c4e003388cb8c5778f8d0457bfde020
Felipe Cecagno <fcecagno@gmail.com>
2021-03-29 00:10:47 -0300

    add a way to run an arbitrary script as bigbluebutton; add playbook to shutdown instances on oci

---
+++ playbooks/files/shutdown-oci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+INSTANCE_ID=$(curl -s http://169.254.169.254/opc/v1/instance/id)
+oci compute instance action --action SOFTSTOP --instance-id ${INSTANCE_ID} --auth instance_principal
