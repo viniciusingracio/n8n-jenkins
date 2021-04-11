f4738efa865259cb96d322f6d3f2463c026e8947
Felipe Cecagno <fcecagno@gmail.com>
2021-04-11 00:44:22 -0300

    new role bigbluebutton-scheduled-shutdown; change way we reset ssh; bump version of sfu-phone and kurento

--- roles/bigbluebutton-scheduled-shutdown/files/shutdown-oci.sh
+++ roles/bigbluebutton-scheduled-shutdown/files/shutdown-oci.sh
@@ -4,4 +4,4 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 INSTANCE_ID=$(curl -s http://169.254.169.254/opc/v1/instance/id)
-oci compute instance action --action SOFTSTOP --instance-id ${INSTANCE_ID} --auth instance_principal
+/usr/local/bin/oci compute instance action --action SOFTSTOP --instance-id ${INSTANCE_ID} --auth instance_principal
